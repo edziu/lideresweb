8dab8e91bcdc5b8f002920e19f178b2d0615b6d0
Eduardo Diaz <eduar.diaz37@gmail.com>
2013-12-12 12:35:29 -0400

    add test, attributes user model

--- test/unit/user_test.js
+++ test/unit/user_test.js
@@ -7,10 +7,28 @@ var UserModel = require('../../app/models').UserModel;
 
 describe('@user model', function () {
     
+    beforeEach(function () {
+        this.user = new UserModel({
+            displayName : 'Eduardo Diaz',
+            userName    : 'EduardoDiaz',
+            email       : 'eduar.diaz37@gmail.com'
+        });
+    });
+
+
     describe('Instance', function () {
         it('user should instance UserModel', function () {
-            var user = new UserModel();
-            expect(user).to.be.instanceOf(UserModel);
+            expect(this.user).to.be.instanceOf(UserModel);
+        });
+
+        it('user should attributes', function () {
+            expect(this.user.get('displayName')).to.be.equal('Eduardo Diaz');
+            expect(this.user.get('userName')).to.be.equal('EduardoDiaz');
+            expect(this.user.get('email')).to.be.equal('eduar.diaz37@gmail.com');
+        });
+
+        it('createAt attribute should be created by default', function () {
+            expect(this.user.get('createAt')).to.be.instanceOf(Date);
         });
     });
 });
